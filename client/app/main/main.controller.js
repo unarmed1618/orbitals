3089dc52300ac3bbc6fb6daf634bc618b2aa72d1
Kalia Jade Darrow <darrowj08@students.ecu.edu>
2016-01-10 22:15:51 -0500

    negatives and decimals

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,21 +7,28 @@ class MainController {
   constructor($scope) {
   	this.$scope = $scope;
     this.canvas = document.getElementById("drawspace");
+    this.$scope.spacing=12;
+    this.$scope.pointSize=3;
 	this.canvasContext = this.canvas.getContext("2d");
 	this.xc=this.canvas.width/2;
 	this.yc=this.canvas.height/2;
-	this.spacing=12;
+	this.spacing=10;
   }
     //Express a positive number as an orbital number.
     drawValue(){
 		this.clear();
 		var localNum = this.$scope.drawNum;
 		var arraychar = localNum.toString().split('');
+		if(arraychar[0]=='-'){
+			this.isNegative=true;
+			arraychar.shift(1);
+		}else
+			this.isNegative=false;
 		//console.log(arraychar);
 		for(var i=0;i<arraychar.length;i++){
 			this.drawPoints(arraychar[i],2*i+1);
 			if(i<arraychar.length-1){
-				this.drawCircle((i*2+2)*this.spacing);
+				this.drawCircle((i*2+2)*this.$scope.spacing,arraychar[i]=='.');
 			}
 		}
     
@@ -33,14 +40,11 @@ class MainController {
     }
 
 	drawPoints(num,radius){
-		//console.log("Attempting to draw " +num+ " points.");
 		if(num==0){}
-		//else if(num==1)
-		//{this.drawPoint(this.xc,this.yc)}
 		else{
 			var radcut = this.divideRadians(num);
 			for(var i=0;i<num;i++){
-				this.drawPoint(this.xc+Math.cos(radcut*i)*this.spacing*radius,this.yc+Math.sin(radcut*i)*this.spacing*radius);
+				this.drawPoint(this.xc+Math.cos(radcut*i)*this.$scope.spacing*radius,this.yc+Math.sin(radcut*i)*this.$scope.spacing*radius);
 			}
 		}
 	}
@@ -51,17 +55,26 @@ class MainController {
 			return 0;
 	}
 	drawPoint(x,y){
-	//console.log("Drawing a point at "+x+", "+y+".");
 	var ctx = this.canvasContext;
 		ctx.beginPath();
-		ctx.arc(x,y,3,0,2*Math.PI);
+		ctx.arc(x,y,this.$scope.pointSize,0,2*Math.PI);
+	if(this.isNegative)
+		ctx.stroke();
+	else
 		ctx.fill();
+		
 	}
-	drawCircle(width){
+	drawCircle(width,isDecimal){
 	var ctx = this.canvasContext;
 		ctx.beginPath();
 		ctx.arc(this.xc,this.yc,width,0,2*Math.PI);
-		ctx.stroke();
+		if(isDecimal){
+			ctx.stroke();
+			ctx.arc(this.xc,this.yc,width-this.$scope.spacing,0,2*Math.PI);
+			ctx.stroke();
+			}
+		else
+			ctx.stroke();
 	}
 
 }
